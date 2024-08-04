4a09afe36ebb7ac219e4b9a7fa6c4576ffb47a9a
Leonel Pedrozo <leonel.pedrozo.quiroz@gmail.com>
2024-08-04 16:41:48 -0500

    Màs ejemplos de funciones lambdas

--- 00_general/01_1_map.py
+++ 00_general/01_1_map.py
@@ -1,28 +1,27 @@
 print()
-
 print(">>> Map")
+print()
+
+print("1-Map sin funcion lambda")
 def cuadrado(numero):
     return numero * numero
+
 lista = [1,2,3,4,5]
 resultado = map(cuadrado, lista)
 lista_resultados = list(resultado)
 print(lista_resultados)
-
 print()
 
-print(">>> Map con funcion lambda")
-
+print("2-Map con funcion lambda")
 lista2 = [1,2,3,4,5,6,7]
 resultado2 = map(lambda numero : numero * numero, lista2)
 lista_resultados2 = list(resultado2)
 print(lista_resultados2)
-
 print()
 
 #Pares
-print(">>>Pares")
+print("3-Pares")
 numbers = range(11)
 even_numbers = list(filter(lambda x: x%2 == 0, numbers))
 print(even_numbers)
-
 print()
--- 04_funciones/02_funcion_lambda.py
+++ 04_funciones/02_funcion_lambda.py
@@ -1,22 +1,34 @@
 print()
-
 print(">>>Funciones lambdas")
 print()
+
+print("1- add = lambda a,b :  a + b:")
 add = lambda a,b :  a + b
 print(add(10,4))
-
 print()
 
+print("2- multiply = lambda a,b : a*b")
 multiply = lambda a,b : a*b
 print(multiply(2,2))
+print()
 
 #Cuadrado de cada numero
 numbers = range(11)
 squared_numbers = list(map(lambda x : x**2, numbers))
+print("3- Lista para armar el cuadrado por cada nùmero")
 print("Cudrados: ",squared_numbers)
-
 print()
 
-#Pares
+print("4- Lista para armar el cuadrado por cada nùmero y filtrar sòlo los pares")
 even_numbers = list(filter(lambda x: x%2 == 0, numbers))
 print(even_numbers)
+print()
+
+print("5- lambda con una variable de texto para concatenar")
+full_name = lambda name, last_name :f"Full name is {name.title()} {last_name.title()}"
+print(full_name('Leonel','Pedrozo'))
+print()
+
+print("6- Llamar directamente la lambda")
+print((lambda x : x * 2)(6))
+print()
