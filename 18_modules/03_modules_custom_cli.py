f6d13e84602bc48b00eabf403e0e1f2243f8a397
Leonel Pedrozo <leonel.pedrozo.quiroz@gmail.com>
2024-08-08 20:33:09 -0500

    Uso de mòdulos como script

--- 18_modules/03_modules_custom_cli.py
+++ 18_modules/03_modules_custom_cli.py
@@ -1,3 +1,11 @@
+import modules_custom_02_cli
+
 print()
 print(">>>Module cli #2")
 print()
+
+print("1- Impresiòn de una variable del mòdulo")
+print(modules_custom_02_cli.data)
+
+if __name__ == "__main__": 
+    print("Ejecucion como script principal desde el cli")
---
+++ 18_modules/modules_custom_02_cli.py
@@ -0,0 +1,36 @@
+import modules_custom
+
+data = [
+        {
+            "Country":"Colombia",
+            "Population":500
+        },
+        {
+            "Country":"Bolivia",
+            "Population":300
+            
+        }
+    ]
+
+def run():
+    print()
+    print(">>>Module cli #1")
+    print()
+
+    print("1- Invocar todo un modulo y un mètodo en particular")
+    print(modules_custom.get_population())
+    print()
+
+    print("2- Invocar todo un modulo y otro mètodo en particular")
+
+    result = modules_custom.get_population_by_country(data,"Colombia")
+    print(result)
+    print()
+
+    print("3- pidiendo el país:")
+    input = input("Digite el país ")
+    result = modules_custom.get_population_by_country(data,input)
+    print(result)
+    print()
+
+
